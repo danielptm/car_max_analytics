0b722e0a65e26e828f61a6efa9f43903811dd6a5
Daniel Tuttle <danieltuttle@Daniels-Mac-Studio.local>
2023-11-23 16:33:18 -0800

    added some more functionality

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ The python interpreter and data is baked into this project. Just follow the belo
 2. Install dataspell https://www.jetbrains.com/dataspell/
 3. Setup virtual env (ask Dan how)
 4. Double click on `main.ipynb` and press green arrow in IDE to run jupyter notebook.
+
+
+Data questions:
+Is price the price of the car that was received after trade?
+
+Analytical Questions:
+Does appraisal_offer value predict price? (assuming price is the value of the car received after trade)
+Is there a difference in beta coeffiecients of the predictions by car model or car brand? If so, which ones are associated with higher betas?
+How does previous model determine liklihood for trade for an SUV?
+What predicts trading for a premium trim?
+Can upgrading from small Suv to large suv be predicted?
+
+
--- notebook_help/cm_main.py
+++ notebook_help/cm_main.py
@@ -14,3 +14,27 @@ def count_car_makes(df: pd.DataFrame):
 def count_car_models(df: pd.DataFrame):
     res = df.groupby("model_appraisal").size()
     return res
+
+
+
+def suv_mean_price(df: pd.DataFrame):
+    filtered = df[df['vehicle_type'].str.contains("SUV")]
+    suv_mean = filtered.loc[:, "price"].mean()
+    return suv_mean
+
+
+def non_suv_mean_price(df: pd.DataFrame):
+    filtered = df[~df['vehicle_type'].str.contains("SUV")]
+    non_suv_mean = filtered.loc[:, "price"].mean()
+    return non_suv_mean
+
+def add_precentile_price_column(df: pd.DataFrame):
+    df['percentile_price'] = pd.qcut(df['price'], 10, labels=False)
+    return df
+
+def add_precentile_appraisal_offer_column(df: pd.DataFrame):
+    df['percentile_appraisal_offer'] = pd.qcut(df['appraisal_offer'], 10, labels=False)
+    return df
+
+
+
